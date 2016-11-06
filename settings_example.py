d8867d509c661f095c6545fd41170208badb7c3d
Michal Skop <michal.skop@kohovolit.eu>
2016-11-06 04:34:37 +0100

    updated settings example

--- settings_example.py
+++ settings_example.py
@@ -1,5 +1,11 @@
-# example settings for Bot
+# settings for scrapers
+fortuna_url = "http://www.ifortuna.cz/cz/sazeni/"
+fortuna_params = "?limit=100"
+fortuna_dirs = ['politika']
+# settings for github
+project_url = "https://raw.githubusercontent.com/michalskop/ifortuna.cz/master/"
+# settings for MakaBot
 git_dir = "/home/user/project/ifortuna.cz/"
 ssh_file = "/home/user/.ssh/ExampleBot"
 bot_name = "ExampleBot"
-bot_email = "ExampleBot@example.com"
+bot_email = "examplebot@example.com"
