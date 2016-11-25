d07826ef113ad2f633f319c4d2d7f356a6b6decf
Michal Skop <michal.skop@kohovolit.eu>
2016-11-25 14:18:28 +0100

    fixing utils

--- ifortuna_cz_scraper_utils.py
+++ ifortuna_cz_scraper_utils.py
@@ -15,48 +15,54 @@ def scrape_dir(fdir):
 
         divs = domtree.xpath('//div[@class="gradient_table"]')
         for div in divs:
-            group = {}
-            group['identifier'] = re.search('bet-table-holder-(\d{1,})',div.xpath('@id')[0]).group(1)
-            group['title'] = ''.join(div.xpath('div/h3')[0].itertext()).strip()
             try:
-                group['title_comment'] = div.xpath('div/p')[0].text
-            except:
-                group['title_comment'] = ""
-            group['title_bet'] = div.xpath('div/div/table/thead/tr[@class="header-row"]/th[@class="col_title_info"]/a')[0].text.strip()
-
-            ths = div.xpath('div/div/table/thead/tr[@class="header-row"]/th[@class="col_bet"]')
-            group['bets'] = []
-            for th in ths:
-                if len(th.xpath('span')) > 0:
-                    group['bets'].append(th.xpath('span')[0].text.strip())
-                else:
-                    group['bets'].append(th.xpath('a')[0].text.strip())
-
-            trs = div.xpath('div/div/table/tbody/tr')
-            rows = []
-            for tr in trs:
-                item = {}
+                group = {}
+                group['identifier'] = re.search('bet-table-holder-(\d{1,})',div.xpath('@id')[0]).group(1)
+                group['title'] = ''.join(div.xpath('div/h3')[0].itertext()).strip()
+                print(group['title'])
                 try:
-                    item['title'] = tr.xpath('td[@class="col_title"]/div/span/a')[0].text.strip()
+                    group['title_comment'] = div.xpath('div/p')[0].text
+                except:
+                    group['title_comment'] = ""
+                group['title_bet'] = div.xpath('div/div/table/thead/tr[@class="header-row"]/th[@class="col_title_info"]/a')[0].text.strip()
+                print(group['title_bet'])
+
+                ths = div.xpath('div/div/table/thead/tr[@class="header-row"]/th[@class="col_bet"]')
+                group['bets'] = []
+                for th in ths:
+                    if len(th.xpath('span')) > 0:
+                        group['bets'].append(th.xpath('span')[0].text.strip())
+                    else:
+                        group['bets'].append(th.xpath('a')[0].text.strip())
+
+                trs = div.xpath('div/div/table/tbody/tr')
+                rows = []
+                for tr in trs:
+                    item = {}
                     try:
-                        item['title'] += " " + tr.xpath('td[@class="col_title"]/div/span/span/span')[0].text.strip()
+                        item['title'] = tr.xpath('td[@class="col_title"]/div/span/a')[0].text.strip()
+                        try:
+                            item['title'] += " " + tr.xpath('td[@class="col_title"]/div/span/span/span')[0].text.strip()
+                        except:
+                            nothing = None
+                        item['bets'] = []
+                        item['identifier'] = tr.xpath('td[@class="col_title"]/div/span/span')[0].text.strip()
+                        ass = tr.xpath('td[@class="col_bet "]/a')
+                        for a in ass:
+                            item['bets'].append("".join(a.itertext()).strip())
+                        item['date_bet'] = ''.join(tr.xpath('td[@class="col_date sorted_column"]/span')[0].itertext()).strip()
+                        rows.append(item)
                     except:
                         nothing = None
-                    item['bets'] = []
-                    item['identifier'] = tr.xpath('td[@class="col_title"]/div/span/span')[0].text.strip()
-                    ass = tr.xpath('td[@class="col_bet "]/a')
-                    for a in ass:
-                        item['bets'].append("".join(a.itertext()).strip())
-                    item['date_bet'] = ''.join(tr.xpath('td[@class="col_date sorted_column"]/span')[0].itertext()).strip()
-                    rows.append(item)
-                except:
-                    nothing = None
-            group['rows'] = rows
-            data.append(group)
+                group['rows'] = rows
+                data.append(group)
+            except:
+                nothing = None
     return data
 
 if __name__ == "__main__":
     # test:
-    fdir = 'fotbal'
-    data = scrape_dir(fdir)
-    print(data)
+    fdirs = ['basketbal','volejbal','fotbal','hokej','tenis-muzi','tenis-zeny','hazena','ameircky-fotbal','ragby','zabava']
+    for fdir in fdirs:
+        data = scrape_dir(fdir)
+        print(data)
